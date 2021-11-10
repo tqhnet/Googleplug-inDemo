e5a4ad36bcc3bc831f223256e00c8e04f0396226
xj_mac <tao.li@spreadwin.com>
2021-11-10 15:51:36 +0800

    更新第一款插件

---
+++ 91_no_ad/scripts/test_ad.js
@@ -0,0 +1,11 @@
+
+// https://www.meiju7.com/*
+console.log("脚本加载成功！！");
+document.addEventListener('DOMContentLoaded', function() {
+	console.log('我被执行了！');
+	setTimeout(function() {
+		document.getElementById('HMcoupletDivleft').style.display = 'none';
+		document.getElementById('HMcoupletDivright').style.display = 'none';
+		document.getElementById('HMRichBox').style.display = 'none';
+	}, 1000);
+});
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Googleplug-inDemo
 谷歌插件的Demo练习汇总
+
+91_no_ad：做的第一款插件去除美剧网站广告
